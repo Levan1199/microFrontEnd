7ffe63c384bd11f13784035fc14b6f96e5587c93
levan1199 <vipchicken28@gmail.com>
2022-02-26 16:42:46 +0700

    added auth subapp

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createMemoryHistory, createBrowserHistory} from 'history';
 // Mount function to start up the app
-const mount = (el) => {
- ReactDOM.render(
-    <App/>,
-    el
- );
+const mount = (el,{onNavigate, defaultHistory, initialPath}) => {
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries: [initialPath]
+    });
+
+    if(onNavigate){
+        history.listen(onNavigate);        
+    }
+    ReactDOM.render ( <App history={history}/>, el );
+    return {
+        onParentNavigate({pathname: nextPathname}){
+            const {pathname} = history.location;
+            if(pathname !== nextPathname){
+                history.push(nextPathname);
+            }
+        }
+    }
 };
 
 if (process.env.NODE_ENV === 'development'){
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if(devRoot){
-        mount(devRoot);
-    }
+        mount(devRoot,{defaultHistory: createBrowserHistory()});
+    } 
 }
 //If we are in development and in isolation then call mount immediately
 
