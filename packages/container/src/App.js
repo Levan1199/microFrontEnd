2d5ddd41e16acb59c5d72f883c9fae48ec40acb0
levan1199 <vipchicken28@gmail.com>
2022-02-27 14:44:26 +0700

    added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,24 +1,39 @@
-import React, {lazy, Suspense, useState} from "react";
+import React, {lazy, Suspense, useEffect, useState} from "react";
 import Header from "./components/Header";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch , Router, Redirect} from "react-router-dom";
 import { StylesProvider, createGenerateClassName  } from "@material-ui/core/styles";
 import Progress from "./components/Progress";
+import {createBrowserHistory} from "history";
+
+
 const MarketingLazy = lazy(()=> import('./components/MarketingApp'));
 const AuthLazy = lazy(()=>import('./components/AuthApp'));
+const DashboardLazy = lazy(()=> import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 });
 
+const history = createBrowserHistory();
+
 export default () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
+    useEffect(()=>{
+        if(isSignedIn){
+            history.push('/dashboard');
+        }
+    },[isSignedIn]);
     return (
-        <BrowserRouter>
+        <Router history={history}>
         <StylesProvider generateClassName={generateClassName}>
             <div>   
                 <Header onSignOut={()=>setIsSignedIn(false)} isSignedIn={isSignedIn}/>
                 <Suspense fallback={<Progress/>}>
-                <Switch>
+                <Switch>                    
+                    <Route path="/dashboard">
+                        {!isSignedIn && <Redirect to="/"/>}
+                        <DashboardLazy />
+                    </Route>
                     <Route path="/auth" >
                         <AuthLazy onSignIn={()=>setIsSignedIn(true)}/>
                     </Route>
@@ -27,6 +42,6 @@ export default () => {
                 </Suspense>
             </div>
         </StylesProvider>
-        </BrowserRouter>
+        </Router>
     );
 };
---
+++ packages/dashboard/config/webpack.dev.js
@@ -0,0 +1,43 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+/*
+This happens because your server does not know what to do when it is hit by the /about route, initially when the browser has no asset files (HTML, JS, etc.). It occurs only when you are at /about and you refresh the page.
+
+React router is a client side router, and hence the browser needs to load the client assets completely before it can work on your routes.
+
+https://stackoverflow.com/questions/68379052/routing-not-working-in-a-react-app-with-webpack
+*/
+
+const devConfig = {
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8083/',
+  },
+  devServer: {
+    port: 8083,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DashboardApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
